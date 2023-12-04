1867263cb99cf2049133bd0f2b0a9462f047fa0a
harish06zoro <harish12ser@gmail.com>
2023-12-04 12:11:44 +0530

    dawn

---
+++ oopsgit1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsgit1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+            student s1 = new student("harish", 1001, 21, "trichy");
+            student s2 = new student("ansu", 1002, 21, "trichy");
+            student s3=  new student("malis", 1003, 21, "trichy");
+
+            s.addstudent(s1);
+            s.addstudent(s2);
+            s.addstudent(s3);
+            s.displaystudent();
+
+        }
+    }
+}
---
+++ oopsgit1/student.cs
@@ -0,0 +1,47 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsgit1
+{
+    public  class student
+    {
+        string name;
+
+        int rollno;
+        int age;
+        string address;
+        List<student> studlist = new List<student>();
+        public student()
+        {
+        }
+        public student(string name,int rollno,int age,string address)
+        {
+            this.name=name;
+            this.rollno = rollno;
+
+            this.age=age;
+            this.address = address;
+
+        }
+        public void addstudent(student stud)
+        {
+            studlist.Add(stud);
+        }
+        public void displaystudent()
+        {
+            foreach (student stud in studlist)
+            {
+                Console.WriteLine(stud);
+            }
+        }
+        public override string ToString()
+        {
+            return this.name + "-" + this.age + "-" + this.rollno + "-" + this.address;
+        }
+        
+
+    }
+}
